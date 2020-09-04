4a3aea601f1c64e3977dec0a27f44a87dd20dacc
urmilshroff <urmilshroff@gmail.com>
2020-09-04 16:58:35 +0530

    Basic Google sign in

---
+++ lib/services/firebase_auth_helper.dart
@@ -0,0 +1,34 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class FirebaseAuthHelper {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  Future<void> signInWithGoogle() async {
+    final GoogleSignInAccount _googleSignInAccount =
+        await _googleSignIn.signIn();
+
+    final GoogleSignInAuthentication _googleSignInAuthentication =
+        await _googleSignInAccount.authentication;
+
+    final AuthCredential _authCredential = GoogleAuthProvider.getCredential(
+      accessToken: _googleSignInAuthentication.accessToken,
+      idToken: _googleSignInAuthentication.idToken,
+    );
+
+    final _authResult =
+        await _firebaseAuth.signInWithCredential(_authCredential);
+
+    final User _user = _authResult.user;
+
+    assert(!_user.isAnonymous);
+    assert(await _user.getIdToken() != null);
+
+    print('${_user.displayName} signed in');
+  }
+
+  void signOutWithGoogle() async {
+    await _googleSignIn.signOut();
+  }
+}
