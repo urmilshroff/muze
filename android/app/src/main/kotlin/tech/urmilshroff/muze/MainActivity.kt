269134c53ad247e7dcb28273120382f6df8a37b3
urmilshroff <urmilshroff@gmail.com>
2020-09-04 21:21:15 +0530

    Revert "Added native splash screen"

--- android/app/src/main/kotlin/tech/urmilshroff/muze/MainActivity.kt
+++ android/app/src/main/kotlin/tech/urmilshroff/muze/MainActivity.kt
@@ -2,8 +2,5 @@ package tech.urmilshroff.muze
 
 import io.flutter.embedding.android.FlutterActivity
 
-import android.os.Build
-import android.view.ViewTreeObserver
-import android.view.WindowManager
 class MainActivity: FlutterActivity() {
 }
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,9 +9,6 @@ import Firebase
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     FirebaseApp.configure()
-    var flutter_native_splash = 1
-    UIApplication.shared.isStatusBarHidden = false
-
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
