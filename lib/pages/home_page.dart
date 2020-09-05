6e52383b080b413efaf89e718d42ce908b3b3e0a
urmilshroff <urmilshroff@gmail.com>
2020-09-05 23:28:24 +0530

    Updated FabCircularMenu

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:fab_circular_menu/fab_circular_menu.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hive/hive.dart';
@@ -6,7 +7,6 @@ import 'package:muze/models/user_model.dart';
 import 'package:muze/utils/colors.dart';
 import 'package:muze/utils/screen_sizes.dart';
 import 'package:muze/utils/text_styles.dart';
-import 'package:muze/widgets/fab_circular_menu.dart';
 import 'package:provider/provider.dart';
 
 class MyHomePage extends StatelessWidget {
@@ -51,7 +51,34 @@ class MyHomePage extends StatelessWidget {
         elevation: 0.0,
       ),
       body: Container(),
-      floatingActionButton: MyFABCircularMenu(),
+      floatingActionButton: FabCircularMenu(
+        fabColor: MyColors.primary,
+        fabOpenColor: MyColors.secondary,
+        fabOpenIcon: Icon(Icons.menu_rounded, color: MyColors.white),
+        fabCloseIcon: Icon(Icons.close_rounded, color: MyColors.white),
+        ringColor: MyColors.primary,
+        ringDiameter: 360.0.w,
+        ringWidth: 60.0.w,
+        fabSize: 56.0,
+        fabElevation: 3.0,
+        children: [
+          IconButton(
+            icon: Icon(Icons.person_add_rounded, color: MyColors.white),
+            iconSize: 30.0.w,
+            onPressed: null,
+          ),
+          IconButton(
+            icon: Icon(Icons.pause_rounded, color: MyColors.white),
+            iconSize: 36.0.w,
+            onPressed: null,
+          ),
+          IconButton(
+            icon: Icon(Icons.play_arrow_rounded, color: MyColors.white),
+            iconSize: 36.0.w,
+            onPressed: null,
+          ),
+        ],
+      ),
     );
   }
 }
